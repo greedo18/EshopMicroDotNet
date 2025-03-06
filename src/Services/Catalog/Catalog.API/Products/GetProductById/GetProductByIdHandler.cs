ce91a43a8bf53b51f28bdf9a25e9f85f8d8c8a36
Pierre Carpentier <Pierre.Carpentier@aero.bombardier.com>
2025-03-05 22:05:20 -0500

    finished GetPRoductById Enpoint

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.API.Products.GetProductById;
+
+public record GetProductbyIdQuery(Guid Id) : IQuery<GetProductByIdResult>;
+
+public record GetProductByIdResult(Product Product);
+
+internal class GetProductByIdQueryHandler
+    (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+    : IQueryHandler<GetProductbyIdQuery, GetProductByIdResult>
+{
+    public async Task<GetProductByIdResult> Handle(GetProductbyIdQuery query, CancellationToken cancellationToken)
+    {
+        logger.LogInformation("GetProductByIdQueryHandler.HAndler caller with {@Query}", query);
+
+        var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+        if (product is null)
+        {
+            throw new ProductNotFoundException();
+        }
+
+        return new GetProductByIdResult(product);
+    }
+}
