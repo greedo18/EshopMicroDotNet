ed3cfb3834fd2a49bcc8a0eff0af255dc34b1a56
Pierre Carpentier <Pierre.Carpentier@aero.bombardier.com>
2025-03-17 21:11:44 -0400

    implemented Global exception handling with IExceptionHandler interface

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -1,12 +1,23 @@
 ﻿
 namespace Catalog.API.Products.UpdateProduct;
 
-public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal price)
+public record UpdateProductCommand(Guid Id, string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     :ICommand<UpdateProductResult>;
 
 public record UpdateProductResult(bool IsSuccess);
 
-internal class UpdateProductCommandHandler
+public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+{
+    public UpdateProductCommandValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty().WithMessage("Product ID is required");
+        RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required")
+            .Length(2, 150).WithMessage("Name must be btw 2 and 150 characters");
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price must be greater than 0");
+    }
+}
+
+    internal class UpdateProductCommandHandler
     (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
     : ICommandHandler<UpdateProductCommand, UpdateProductResult>
 {
@@ -18,14 +29,14 @@ internal class UpdateProductCommandHandler
 
         if (product is null)
         {
-            throw new ProductNotFoundException();
+            throw new ProductNotFoundException(command.Id);
         }
 
         product.Name = command.Name;
         product.Category = command.Category;
         product.Description = command.Description;
         product.ImageFile = command.ImageFile;
-        product.Price = command.price;
+        product.Price = command.Price;
 
         session.Update(product);
         await session.SaveChangesAsync(cancellationToken);
